52b53f76bda3a95f89ce83a4ff94df4050bcff2a
Bobrys <431.bobrys@gmail.com>
2025-05-04 15:43:02 +0200

    feat: add functions for statistical calculations
    Implemented three basic statistical functions in statistics.py: average, median, and standard deviation.

---
+++ statistics.py
@@ -0,0 +1,59 @@
+def average(sequence):
+    """
+    Calculates the average of a given sequence of numbers.
+
+    This function takes a sequence of numeric values and computes their average.
+    If the given sequence is empty, the function returns None.
+
+    :param sequence: A sequence of numeric values (e.g., list, tuple).
+    :return: The average of the numbers in the sequence if non-empty,
+        otherwise returns None.
+    """
+    if len(sequence) == 0: return None
+
+    return sum(sequence) / len(sequence)
+
+def median(sequence):
+    """
+    Calculates the median value of a given sequence of numbers.
+
+    The median is the value separating the higher half from the lower half of a
+    data set. If the sequence has an even number of elements, the median is defined
+    as the average of the two middle numbers. If it has an odd number of elements,
+    the median is the middle value in the sorted sequence.
+
+    :param sequence: A sequence of numeric values for which the median needs to
+        be calculated.
+    :type sequence: list[float] | list[int]
+    :return: The median value of the sequence if it is non-empty, otherwise None.
+    :rtype: float | int | None
+    """
+    length = len(sequence)
+    if length == 0: return None
+
+    sequence = sorted(sequence)
+    if (length % 2) == 0:
+        return average(sequence[length // 2 - 1:length // 2 + 1])
+    else:
+        return sequence[length // 2]
+
+def deviation(sequence):
+    """
+    Calculate the sample standard deviation of a given sequence.
+
+    This function computes the sample standard deviation, which quantifies the
+    amount of variation or dispersion in a given sequence of numeric values. The
+    calculation uses the average (mean) of the sequence and ensures the result
+    is unbiased by dividing by `(n - 1)` for a sequence of length `n`.
+
+    :param sequence: A list or any iterable of numeric values.
+    :return: The sample standard deviation if the sequence has at least two
+        elements. Returns 0 if the sequence contains exactly one element. Returns
+        None if the sequence is empty.
+    """
+    length = len(sequence)
+    avg = average(sequence)
+    if length == 0: return None
+    if length == 1: return 0
+
+    return (sum((i - avg) ** 2 for i in sequence) / (length - 1)) ** 0.5
