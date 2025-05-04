6e8c214cd4a576882cb29eeb6dcd79f3a195ed6f
Bobrys <431.bobrys@gmail.com>
2025-05-04 17:13:45 +0200

    refactor: update project structure and add files for methods showcase
    The whole structure has been updated. All files with mathematical calculation methods have moved to 'mathalgs' package. Added separated file for showcasing methods and another with example data.

---
+++ mathalgs/__init__.py
@@ -0,0 +1,3 @@
+from .statistics import average
+from .statistics import median
+from .statistics import deviation
---
+++ showcase.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import mathalgs as ma
+
+df = pd.read_csv("data.txt", delimiter=r'\s+')
