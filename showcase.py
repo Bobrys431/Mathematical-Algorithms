3ea647c3fd377db56be7696ac3af625477a5fff1
Bobrys <431.bobrys@gmail.com>
2025-05-05 21:40:59 +0200

    feat: visualize functions from data.txt in showcase.py
    Executing showcase.py now generates a matplotlib plot of all functions defined in data.txt. Each function is plotted for all separated values of y based on the dependency x : f(x, y).

--- showcase.py
+++ showcase.py
@@ -1,4 +1,24 @@
-import pandas as pd
-import mathalgs as ma
+import mathalgs as ma  # Import custom math algorithms module
+import pandas as pd  # Import pandas for data manipulation
+import matplotlib.pyplot as plt  # Import matplotlib for plotting
 
+
+
+# Read data from the text file
 df = pd.read_csv("data.txt", delimiter=r'\s+')
+
+
+
+# Create a new figure
+plt.figure("Chart F(x,y) for every y")
+plt.title("Chart F(x,y) for every y")
+
+# Group data by 'y' values and plot each group
+for value, subdf in df.groupby('y'):
+    plt.plot(subdf['x'], subdf['f(x,y)'], label="y = " + str(value))
+
+# Set labels and show the plot
+plt.xlabel("x")
+plt.ylabel("F(x,y)")
+plt.legend()
+plt.show()
