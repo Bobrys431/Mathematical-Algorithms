b76db8ab8ede574261440d6bfd8b0651c9fe4d0e
Bobrys <431.bobrys@gmail.com>
2025-05-10 17:01:28 +0200

    refactor: improve code style in spline interpolation function
    Refactored the cubic spline interpolation function to improve code readability and structure. Now uses mathalgs.statistics to calculate standard deviation and average.

--- mathalgs/interpolate.py
+++ mathalgs/interpolate.py
@@ -1,3 +1,4 @@
+from mathalgs import statistics as st
 import numpy as np
 
 def polynomial(xs, ys, interpolating_xs):
@@ -41,40 +42,39 @@ def polynomial(xs, ys, interpolating_xs):
 
 def spline(xs, ys, interpolating_xs):
     """
-    Performs cubic spline interpolation on a set of data points and computes the interpolated
-    values for given x-values. A spline is generated with added boundary conditions for smooth
-    curve transitions. The input x-values must be equally spaced, otherwise an exception will
-    be raised.
+    Calculates spline interpolation for a given set of points and interpolates values
+    for specified x-coordinates. This function implements a cubic B-spline interpolation
+    method. It ensures that x values are evenly spaced and that the input x and y data
+    have the same length.
 
-    :param xs: The x-coordinates of the data points, used for spline interpolation.
-    :type xs: numpy.ndarray
-    :param ys: The y-coordinates of the data points corresponding to `xs`.
-    :type ys: numpy.ndarray
-    :param interpolating_xs: The x-coordinates at which the interpolated y-values are to be computed.
-    :type interpolating_xs: numpy.ndarray
-    :return: The interpolated y-values corresponding to `interpolating_xs`.
-    :rtype: numpy.ndarray
-    :raises ValueError: If the sizes of `xs` and `ys` are unequal.
-    :raises ValueError: If the x-coordinates in `xs` are not equally spaced.
+    :param xs: List or array of x-coordinates (evenly spaced).
+    :param ys: List or array of y-coordinates corresponding to `xs`.
+    :param interpolating_xs: List or array of x-coordinates at which to interpolate.
+    :return: List or array of interpolated y-coordinates corresponding to
+             `interpolating_xs`.
+
+    :raises ValueError: If the number of x and y values are different.
+    :raises ValueError: If the x values are not equally spaced.
     """
     if len(xs) != len(ys):
         raise ValueError("A different number of x and y values were given")
-    if np.std(np.diff(xs)) > 0.01:
+    if st.deviation(np.diff(xs)) > 0.01:
         raise ValueError("Given x values are not equally spaced")
 
-    diff = np.average(np.diff(xs))
+    diff = st.average(np.diff(xs))
     length = len(xs) + 2
 
     splines = np.zeros((length, length))
     np.fill_diagonal(splines, 4)
     np.fill_diagonal(splines[1:], 1)
-    np.fill_diagonal(splines[:,1:], 1)
-    splines[0,0] = -3/diff
-    splines[0,1] = 0
-    splines[0,2] = 3/diff
-    splines[length - 1, length - 3] = -3/diff
-    splines[length - 1, length - 2] = 0
-    splines[length - 1, length - 1] = 3/diff
+    np.fill_diagonal(splines[:, 1:], 1)
+
+    splines[0, 0] = -3 / diff
+    splines[0, 1] = 0
+    splines[0, 2] = 3 / diff
+    splines[-1, -3] = -3 / diff
+    splines[-1, -2] = 0
+    splines[-1, -1] = 3 / diff
 
     expanded_ys = np.insert(ys, 0, 0)
     expanded_ys = np.append(expanded_ys, 0)
@@ -86,19 +86,30 @@ def spline(xs, ys, interpolating_xs):
         expanded_xs = np.insert(expanded_xs, 0, xs[0] - (i + 1) * diff)
         expanded_xs = np.append(expanded_xs, xs[-1] + (i + 1) * diff)
 
+    def basis_value(index, x):
+        x0 = expanded_xs[index - 2]
+        x1 = expanded_xs[index - 1]
+        x2 = expanded_xs[index]
+        x3 = expanded_xs[index + 1]
+        x4 = expanded_xs[index + 2]
+
+        if x0 <= x < x1:
+            return ((x - x0) ** 3) / diff ** 3
+        elif x1 <= x < x2:
+            dx = x - x1
+            return (diff ** 3 + 3 * diff ** 2 * dx + 3 * diff * dx ** 2 - 3 * dx ** 3) / diff ** 3
+        elif x2 <= x < x3:
+            dx = x3 - x
+            return (diff ** 3 + 3 * diff ** 2 * dx + 3 * diff * dx ** 2 - 3 * dx ** 3) / diff ** 3
+        elif x3 <= x <= x4:
+            return ((x4 - x) ** 3) / diff ** 3
+        else:
+            return 0
+
     interpolating_ys = np.zeros(len(interpolating_xs))
+
     for i in range(len(interpolating_xs)):
         for j in range(len(coefficients)):
-            jx = j + 2
-            if expanded_xs[jx - 2] <= interpolating_xs[i] <= expanded_xs[jx - 1]:
-                interpolating_ys[i] += coefficients[j] * (interpolating_xs[i] - expanded_xs[jx - 2])**3 / diff**3
-            elif expanded_xs[jx - 1] <= interpolating_xs[i] <= expanded_xs[jx]:
-                interpolating_ys[i] += coefficients[j] * (diff**3 + 3 * diff**2 * (interpolating_xs[i] - expanded_xs[jx - 1]) + 3 * diff * (interpolating_xs[i] - expanded_xs[jx - 1])**2 - 3 * (interpolating_xs[i] - expanded_xs[jx - 1])**3) / diff**3
-            elif expanded_xs[jx] <= interpolating_xs[i] <= expanded_xs[jx + 1]:
-                interpolating_ys[i] += coefficients[j] * (diff**3 + 3 * diff**2 * (expanded_xs[jx + 1] - interpolating_xs[i]) + 3 * diff * (expanded_xs[jx + 1] - interpolating_xs[i])**2 - 3 * (expanded_xs[jx + 1] - interpolating_xs[i])**3) / diff**3
-            elif expanded_xs[jx + 1] <= interpolating_xs[i] <= expanded_xs[jx + 2]:
-                interpolating_ys[i] += coefficients[j] * (expanded_xs[jx + 2] - interpolating_xs[i])**3 / diff**3
-            else:
-                interpolating_ys[i] += 0
+            interpolating_ys[i] += coefficients[j] * basis_value(j + 2, interpolating_xs[i])
 
     return interpolating_ys
